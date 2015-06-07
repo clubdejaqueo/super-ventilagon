d80df96a60365fce4b8152d6eba6ed01994ac8fa
Alejandro J. Cura <alecu@protocultura.net>
2015-06-07 18:28:14 -0300

    Arreglos para el ultimo commit que funcionaba, antes del big refactor

--- ventilagon_rotor/ventilagon_rotor.ino
+++ ventilagon_rotor/ventilagon_rotor.ino
@@ -22,9 +22,9 @@ int drift_speed = 1;
 class Pattern {
 public:
   byte len;
-  prog_uchar* rows;
+  const char PROGMEM* rows;
   
-  Pattern(byte len, prog_uchar* rows) : len(len), rows(rows) {
+  Pattern(byte len, const char PROGMEM* rows) : len(len), rows(rows) {
   }
   
   inline byte get_row(byte offset) {
@@ -38,7 +38,7 @@ public:
 // times 6 possible rotations
 // times 2 mirrored options = 768 bytes
 
-PROGMEM prog_uchar transformations[] = {
+const char PROGMEM transformations[] = {
   // Mirrored: no
   // Rotated: 0
   0b000000, 0b000001, 0b000010, 0b000011, 0b000100, 0b000101, 0b000110, 0b000111, 0b001000, 0b001001, 0b001010, 0b001011, 0b001100, 0b001101, 0b001110, 0b001111, 0b010000, 0b010001, 0b010010, 0b010011, 0b010100, 0b010101, 0b010110, 0b010111, 0b011000, 0b011001, 0b011010, 0b011011, 0b011100, 0b011101, 0b011110, 0b011111, 0b100000, 0b100001, 0b100010, 0b100011, 0b100100, 0b100101, 0b100110, 0b100111, 0b101000, 0b101001, 0b101010, 0b101011, 0b101100, 0b101101, 0b101110, 0b101111, 0b110000, 0b110001, 0b110010, 0b110011, 0b110100, 0b110101, 0b110110, 0b110111, 0b111000, 0b111001, 0b111010, 0b111011, 0b111100, 0b111101, 0b111110, 0b111111,
@@ -68,7 +68,7 @@ PROGMEM prog_uchar transformations[] = {
 };
 
 // 20 rows
-PROGMEM prog_uchar pat0[] = {
+const char PROGMEM pat0[] = {
   0B000000, 0B000000, 0B000000, 0B000000, 
   0B100111, 0B100111, 0B100111, 0B100111, 
   0B110011, 0B110011, 0B110011, 0B110011, 
@@ -92,7 +92,7 @@ PROGMEM prog_uchar pat0[] = {
 };
 
 // 12 rows
-PROGMEM prog_uchar pat1[] = {
+const char PROGMEM pat1[] = {
   0B000000, 0B000000, 0B000000, 0B000000, 
   0B101111, 0B101111, 0B101111, 0B101111, 
   0B000000, 0B000000, 0B000000, 0B000000, 
@@ -108,7 +108,7 @@ PROGMEM prog_uchar pat1[] = {
 };
 
 // 11 rows
-PROGMEM prog_uchar pat2[] = {
+const char PROGMEM pat2[] = {
   0B000000, 0B000000, 0B000000, 0B000000, 
   0B110110, 0B110110, 0B110110, 0B110110, 
   0B010010, 0B010010, 0B010010, 0B010010, 
@@ -123,7 +123,7 @@ PROGMEM prog_uchar pat2[] = {
 };
 
 // 10 rows
-PROGMEM prog_uchar pat3[] = {
+const char PROGMEM pat3[] = {
   0B000000, 0B000000, 0B000000, 0B000000, 
   0B000000, 0B000000, 0B000000, 0B000000, 
   0B010010, 0B010010, 0B010010, 0B010010, 
@@ -137,7 +137,7 @@ PROGMEM prog_uchar pat3[] = {
 };
 
 // 9 rows
-PROGMEM prog_uchar pat4[] = {
+const char PROGMEM pat4[] = {
   0B000000, 0B000000, 0B000000, 0B000000, 
   0B000001, 0B000001, 0B000001, 0B000001, 
   0B000010, 0B000010, 0B000010, 0B000010, 
