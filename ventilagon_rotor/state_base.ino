b9f71d869c6742cfd069df9689900ea6facc2d36
Alejandro J. Cura <alecu@protocultura.net>
2015-06-07 17:43:47 -0300

    Revert 0bc57b2..aa79591
    This rolls back to commit 0bc57b28167a1d9ececdfaceb882f08fa5c58ec9.

--- ventilagon_rotor/state_base.ino
+++
@@ -1,9 +0,0 @@
-State* State::current_state;
-
-void State::change_state (State* new_state) {
-#ifdef DEBUG
-  Serial.println(new_state->name());
-#endif
-  State::current_state = new_state;
-  State::current_state->setup();
-}
